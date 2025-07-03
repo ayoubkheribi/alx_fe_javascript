e73ca01b863e8cdb0cc6af89cba5a02cddfc33c8
Ayoub <akhribi627@gmail.com>
2025-07-03 21:17:13 +0100

    add the html structure

---
+++ dom-manipulation/script.js
@@ -0,0 +1,24 @@
+const quote = document.getElementById("quoteDisplay");
+const quoteBtn = document.getElementById("newQuote");
+
+let randomQ = [
+  {
+    text: "The important thing is not to stop questioning.",
+    category: "Science"
+  },
+  {
+    text: "First, solve the problem. Then, write the code.",
+    category: "Programming"
+  },
+  {
+    text: "Education is the most powerful weapon you can use to change the world.",
+    category: "Inspiration"
+  },
+  {
+    text: "An investment in knowledge pays the best interest.",
+    category: "Wisdom"
+  }
+];
+document.addEventListener('click', function showRandomQuote() {
+  
+})
